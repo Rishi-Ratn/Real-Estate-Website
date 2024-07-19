334ab871fc207902a54a973ddd3775c6e10ec59b
Rishi_Ratn <imrishiratn009@gmail.com>
2024-07-19 17:35:26 +0530

    complete update listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing} from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/create', verifyToken, createListing);       // verify token to verify user is authenticated or not 
 router.delete('/delete/:id', verifyToken, deleteListing); 
 router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);                   // every one can get the listing, so don't make it secure 
 
 export default router;
