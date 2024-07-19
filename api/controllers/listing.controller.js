f1ab8b4e6f881a4af963cd5405b6d16243ed55b7
Rishi_Ratn <imrishiratn009@gmail.com>
2024-07-19 16:02:07 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
@@ -28,3 +29,21 @@ export const deleteListing = async (req, res, next) => {
     }
 };
 
+export const updateListing = async (req, res, next) => {
+     // first check if listing exists or not
+     const listing = await Listing.findById(req.params.id);
+     if(!listing) {
+         return next(errorHandler(404, 'Listing not found!'));
+     }
+     // now if listing exists, check if the user is the owner of the listing
+     if(req.user.id !== listing.userRef){
+         return next(errorHandler(401, 'You can only update your own listing!'));
+     }
+     // if everything is ok, update the listing
+     try {
+         const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {new: true}); // updated with req.body whatever we sent, new true -> get the update one
+         res.status(200).json(updatedListing);
+     } catch (error) {
+         next(error);
+     }
+};
